e6ad7e33c33e886ed83a97a7e9d01a86a61a5974
bjong <biebergong@gmail.com>
2017-09-19 18:24:03 +0800

    keeplive

--- fuckAnywhere.py
+++ fuckAnywhere.py
@@ -1,14 +1,19 @@
 import socket
 import json
+import time
+import threading
 
 
 def ask_nat_type(client, serv, serv2):
     client.sendto(b'', serv2)
+
+    time.sleep(0.5)
     query_data_dict = {
         'method': 'askNATType'
     }
-    query_data = json.dumps(query_data_dict).encode()
-    client.sendto(query_data, serv)
+    # query_data = json.dumps(query_data_dict).encode()
+    # client.sendto(query_data, serv)
+    dict_sendto(client, query_data_dict, serv)
     result_bytes, _ = client.recvfrom(512)
     result = result_bytes.decode()
     return result == 'true'
@@ -18,12 +23,25 @@ def get_client_id(client, serv):
     query_data_dict = {
         'method': 'getId'
     }
-    query_data = json.dumps(query_data_dict).encode()
-    client.sendto(query_data, serv)
+    # query_data = json.dumps(query_data_dict).encode()
+    # client.sendto(query_data, serv)
+    dict_sendto(client, query_data_dict, serv)
     result_bytes, _ = client.recvfrom(512)
     client_id = int(result_bytes.decode())
     return client_id
     # return result == 'true'
+
+
+def dict_sendto(client, data_dict, serv):
+    data = json.dumps(data_dict).encode()
+    client.sendto(data, serv)
+
+
+def keeplive(client, serv):
+    while True:
+        time.sleep(10)
+        # get_client_id(client, serv)
+        client.sendto(b'', serv)
 
 
 def listen(client, serv):
@@ -45,12 +63,13 @@ def connect_remote(client, serv, remote_id):
         'method': 'connect',
         'id': remote_id
     }
-    query_data = json.dumps(query_data_dict).encode()
-    client.sendto(query_data, serv)
+    # query_data = json.dumps(query_data_dict).encode()
+    # client.sendto(query_data, serv)
+    dict_sendto(client, query_data_dict, serv)
     result_bytes, _ = client.recvfrom(512)
     result = json.loads(result_bytes.decode())
     remote_addr = (result.get('ip'), result.get('port'))
-    print(client.recvfrom(512))
+    # print(client.recvfrom(512))
     while True:
         client.sendto(input().encode(), remote_addr)
         print(client.recvfrom(512))
@@ -58,8 +77,8 @@ def connect_remote(client, serv, remote_id):
 
 def connect_serv():
     client = socket.socket(2, 2)
-    serv = ('127.0.0.1', 2333)
-    serv2 = ('127.0.0.1', 2334)
+    serv = ('121.42.185.92', 2333)
+    serv2 = ('121.42.185.92', 2334)
     if not ask_nat_type(client, serv, serv2):
         print('Can not p2p.')
         return
@@ -68,6 +87,7 @@ def connect_serv():
         print('我的ID:', client_id)
         mode = input('选择模式(0:监听模式；1:主动连接模式):')
         if mode == '0':
+            threading.Thread(target=keeplive, args=(client, serv,)).start()
             listen(client, serv)
         elif mode == '1':
             remote_id = int(input('要连接的ID：'))
