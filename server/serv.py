97f94dc364a00bc90e46596845fe2f9e7cd15044
bj <biebergong@gmail.com>
2021-10-11 15:35:45 +0800

    server support mutil services

--- server/serv.py
+++ server/serv.py
@@ -2,6 +2,7 @@ import socket
 from datetime import datetime
 import requests
 from threading import Thread
+from dataclasses import dataclass
 import config
 
 
@@ -10,41 +11,61 @@ def log(*args):
     print(time, *args)
 
 
-def call_webhook(cfg, host, port):
-    r = requests.request(cfg.method, cfg.url, headers=cfg.headers, json=cfg.json(host, port))
+def call_webhook(cfg, service_name, host, port):
+    r = requests.request(cfg.method, cfg.url, headers=cfg.headers, json=cfg.json(service_name, host, port))
     log('call webhook:', cfg.name, 'result:', r.status_code)
-    return r.status_code == 200
+    return str(r.status_code).startswith('2')
+
+
+@dataclass
+class Service:
+    webhooks: [config.WebhookConfig]
+    name: str
+    port: int = 0
+
+    def set_flags(self, flag):
+        for w in self.webhooks:
+            w.flag = flag
+
+    def check_flags(self, host, port):
+        for w in self.webhooks:
+            if w.flag and call_webhook(w, self.name, host, port):
+                w.flag = False
 
 
 class Server:
     def __init__(self):
-        self.listen_addr = ('0.0.0.0', 4040)
-        self.port = 0
+        self.listen_addr = ('0.0.0.0', config.listen_port)
         self.s = None
-        self.flags = [False for _ in config.webhooks]
 
-        log('listen on', self.listen_addr)
+        self.services = {}
+        for service_name, webhooks in config.webhooks_config.items():
+            service = Service(webhooks=webhooks, name=service_name)
+            self.services[service_name] = service
+        log(self.services)
 
-    def handle(self, data, addr):
-        host, new_port = addr
-        if data != b'ping':
+    def handle(self, raw, addr):
+        data = raw.decode()
+        if not data.startswith('ping'):
             return
 
-        log('recv ping, host:', host, 'port:', new_port)
+        host, port = addr
+        _, service_name = data.split(' ')
+        log('recv ping, service:', service_name, 'host:', host, 'port:', port)
 
-        if self.port == new_port:
-            return
-        else:
-            self.port = new_port
-            self.flags = [True for _ in config.webhooks]
+        service = self.services[service_name]
+        old_port = service.port
+        if old_port != port:
+            service.set_flags(True)
+            service.port = port
 
-        for i, flag in enumerate(self.flags):
-            if flag and call_webhook(config.webhooks[i], host, new_port):
-                self.flags[i] = False
+        for service in self.services.values():
+            service.check_flags(host, port)
 
     def run(self):
         self.s = socket.socket(2, 2)
         self.s.bind(self.listen_addr)
+        log('listen on', self.listen_addr)
 
         while True:
             data, addr = self.s.recvfrom(512)
